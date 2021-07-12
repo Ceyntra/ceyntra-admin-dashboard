fd40cbd59f9b98990a2ac1108346b22039832f50
shehan82 <shehansandeepa82@gmail.com>
2021-07-13 00:56:59 +0530

    added login logout functions

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import './App.css';
-import React  from 'react'
-import Login from './Login';
-import Dashboard from './pages/Dashboard';
-import {BrowserRouter as Router , Route, Switch} from 'react-router-dom';
-import NavBar from './components/navbar/NavBar';
-import TopBar from './components/navbar/TopBar';
+import "./App.css";
+import React from "react";
+import Login from "./Login";
+import Dashboard from "./pages/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/navbar/NavBar";
+import TopBar from "./components/navbar/TopBar";
 
 // Pages
-import Traveller from './pages/Traveller';
-import Places from './pages/Places';
-import Service from './pages/Service';
-import Hotel from './pages/Hotel';
-import Taxi from './pages/Taxi';
-import Guide from './pages/Guide';
-import Message from './pages/Message';
-import Profile from './pages/Profile';
-import { AppBar } from '@material-ui/core';
-
+import Traveller from "./pages/Traveller";
+import Places from "./pages/Places";
+import Service from "./pages/Service";
+import Hotel from "./pages/Hotel";
+import Taxi from "./pages/Taxi";
+import Guide from "./pages/Guide";
+import Message from "./pages/Message";
+import Profile from "./pages/Profile";
+import { AppBar } from "@material-ui/core";
+import Forbidden from "./pages/Forbidden";
+import { LoginProvider } from "./states/LoginContext";
 
 function App() {
   return (
     <div className="App">
+      <LoginProvider>
         <Router>
           <div>
-            <Route exact path='/' component={Login} />
-            <Route path='/home'>
-                <TopBar/>
-                 <NavBar url={window.location.pathname}/>
-              <div className='page-content'>
+            <Route exact path="/" component={Login} />
+            <Route path="/forbidden" component={Forbidden} />
+            <Route path="/home">
+              <TopBar />
+              <NavBar url={window.location.pathname} />
+              <div className="page-content">
                 <Switch>
-                    <Route exact path='/home' component={Dashboard} />
-                    <Route exact path='/home/travellers' component={Traveller} />
-                    <Route path='/home/places' component={Places} />
-                    <Route path='/home/services' component={Service} />
-                    <Route path='/home/hotels' component={Hotel} />
-                    <Route path='/home/taxis' component={Taxi} />
-                    <Route path='/home/guides' component={Guide} />
-                    <Route path='/home/messages' component={Message} />
-                    <Route path='/home/profile' component={Profile} />
+                  <Route exact path="/home" component={Dashboard} />
+                  <Route exact path="/home/travellers" component={Traveller} />
+                  <Route path="/home/places" component={Places} />
+                  <Route path="/home/services" component={Service} />
+                  <Route path="/home/hotels" component={Hotel} />
+                  <Route path="/home/taxis" component={Taxi} />
+                  <Route path="/home/guides" component={Guide} />
+                  <Route path="/home/messages" component={Message} />
+                  <Route path="/home/profile" component={Profile} />
                 </Switch>
               </div>
-              
             </Route>
           </div>
         </Router>
+      </LoginProvider>
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from "react";
+import { createContext } from "react";
+import { useContext } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "./service/axios";
+import { LoginContext } from "./states/LoginContext";
 
 function Login() {
-    return (
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [loginError, setloginError] = useState(null);
+  const history = useHistory();
+
+  // login function
+  var login = async () => {
+    await axios
+      .post("/login", {
+        email: email,
+        password: password,
+      })
+      .then((res1) => {
+        var response = res1.data;
+        if (response == "emailOrPasswordIsIncorrect") {
+          setloginError("emailOrPasswordIsIncorrect");
+        } else if (response == "userNotFound") {
+          setloginError("userNotFound");
+        } else {
+          axios
+            .post(`/encryptDetails`, {
+              userId: response,
+            })
+            .then((res2) => {
+              sessionStorage.setItem("token", res2.data);
+
+              history.push("/home");
+            });
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login page</h1>
+
+      {loginError != null ? (
+        <div>{loginError}</div>
+      ) : (
         <div>
-            <h1>Login page</h1>
+          <h1>sdjfsk</h1>
         </div>
-    )
+      )}
+      <input
+        type="text"
+        name=""
+        id=""
+        autoComplete="true"
+        placeholder="enter username or email"
+        onChange={(e) => {
+          setemail(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="enter password"
+        onChange={(e) => {
+          setpassword(e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <button onClick={login}>Login</button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
