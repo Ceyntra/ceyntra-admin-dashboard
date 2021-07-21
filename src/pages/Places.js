827515e00bad439d9bc6c2b88856ad0662b3e93c
deeghayu <deefernando6@gmail.com>
2021-07-21 17:15:07 +0530

    Update Places.js

--- src/pages/Places.js
+++ src/pages/Places.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
+import "../css/hotel.css";
+import beach from "../assets/images/beach.jpeg";
 
-function Places() {
-    return (
-        <div>
-            <h1>Places page</h1>
+function Hotel() {
+  return (
+    <div>
+      <div className = "hotel1">
+        <div className = "hotel1maintopic">
+          <h3>Hotels with complains</h3>
         </div>
-    )
+        <div className = "hotel1left">
+          <table className = "hotel1table">
+            <tr>
+              <td className = "hotel1tabletopic">Place Name</td>
+              <td>Thalpe Beach</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added by</td>
+              <td>N.M. Perera</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added date</td>
+              <td>2021.07.04</td>
+            </tr>
+          </table>
+          <img className="beach-img" src={beach} alt="" srcset="" height="110px" width="110px"/>
+        </div>
+        <div className = "hotel1right">
+        <table className = "hotel1table">
+            <tr>
+              <td className = "hotel1tabletopic">Place Name</td>
+              <td>Thalpe Beach</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added by</td>
+              <td>N.M. Perera</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added date</td>
+              <td>2021.07.04</td>
+            </tr>
+          </table>
+          <img className="beach-img" src={beach} alt="" srcset="" height="110px" width="110px"/>
+        </div>
+        <div className = "hotel1left">
+        <table className = "hotel1table">
+            <tr>
+              <td className = "hotel1tabletopic">Place Name</td>
+              <td>Thalpe Beach</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added by</td>
+              <td>N.M. Perera</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added date</td>
+              <td>2021.07.04</td>
+            </tr>
+          </table>
+          <img className="beach-img" src={beach} alt="" srcset="" height="110px" width="110px"/>
+          
+        </div>
+        <div className = "hotel1right">
+        <table className = "hotel1table">
+            <tr>
+              <td className = "hotel1tabletopic">Place Name</td>
+              <td>Thalpe Beach</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added by</td>
+              <td>N.M. Perera</td>
+            </tr>
+            <tr>
+              <td className = "hotel1tabletopic">Added date</td>
+              <td>2021.07.04</td>
+            </tr>
+          </table>
+          <img className="beach-img" src={beach} alt="" srcset="" height="110px" width="110px"/>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Places
+export default Hotel;
