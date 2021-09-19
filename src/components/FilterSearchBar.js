a9f80800654a8660813581184f0ff91ef15fb5c7
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-19 12:28:16 +0530

    search bar component

---
+++ src/components/FilterSearchBar.js
@@ -0,0 +1,27 @@
+import React, {useRef} from 'react';
+import { Search} from '@material-ui/icons'
+
+function FilterSearchBar(props) {
+    const inputEl=useRef();
+
+    const getSearchedWord=()=>{
+        const string=inputEl.current.value;
+        props.filterFunction(string);
+    }
+
+    return(
+        <div className="filter-bar">
+            <div >
+                <Search  />
+            </div>  
+            <form noValidate autoComplete="off" >
+                <input classname="input-field" type="text" id="outlined-basic"  variant="outlined" placeholder="Search hotel here..."  size="small" fullWidth 
+                    onChange={getSearchedWord}
+                    ref={inputEl}
+                />
+            </form>
+        </div>
+    );
+}
+
+export default FilterSearchBar;
