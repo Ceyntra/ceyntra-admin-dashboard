fef1b9e9ab5b50db37b331d2c7a8a98e323b6f46
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-19 12:31:08 +0530

    done

--- src/components/hotel/provinceButton.js
+++ src/components/hotel/provinceButton.js
@@ -1,10 +1,21 @@
 import "../../css/provinceButton.css";
 import React from "react";
+import { Link } from "react-router-dom";
+
 function ProvinceButton(props) {
   return (
     <div className="hotelsSection">
       {props.provinces.map((province) => (
-        <button>{province}</button>
+        <Link
+          to={{
+            pathname: `/home/hotels/${province}`,
+            state: {
+              data: province
+            }
+          }}
+        >
+          <button>{province}</button>
+        </Link>
       ))}
     </div>
   );
