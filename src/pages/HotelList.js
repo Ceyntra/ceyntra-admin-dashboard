54e58547aea954f14db254f65aff5fd6da2e4403
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-20 17:00:37 +0530

    added a search bar

--- src/pages/HotelList.js
+++ src/pages/HotelList.js
@@ -2,13 +2,30 @@ import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from
 import React, {useState, useEffect} from 'react';
 import axios from "../service/axios";
 import '../css/hotelList.css';
+import FilterSearchBar from '../components/FilterSearchBar';
 
 function HotelList(){
     const [hotelList, setHotelList] = useState([])
 
+    const [searchedWord, setSearchedWord]=useState("");
+    const [searchedResult, setSearchedResult] = useState([]);
+
+    const filterHandler=(term)=>{
+        if(term !== ""){
+            setSearchedWord(term.toLowerCase());
+            const newHotelList= hotelList.filter((hotel) =>{
+                return hotel['name'].toLowerCase().startsWith(term)
+            })
+            setSearchedResult(newHotelList);
+        }else{
+            setSearchedResult(hotelList);
+        }
+    }
+
     var getHotelDetails = () => {
         axios.get(`/getHotelsBriefDetails`).then((result) => {
           setHotelList(result.data);
+          setSearchedResult(result.data);
         });
     };
     
@@ -22,26 +39,32 @@ function HotelList(){
                 hotelList.length==0 
                     ? <h1>No hotels to be desplayed yet...</h1>
                     : 
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>HOTEL</th>
-                                <th>TELEPHONE</th>
-                                <th>EMAIL</th>
-                                <th>DISTRICT</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {hotelList.map((hotel) => (
-                                <tr key={hotel['id']}>
-                                    <td>{hotel['name']}</td>
-                                    <td>{hotel['userContactModel']['telephone']}</td>
-                                    <td>{hotel['userContactModel']['email']}</td>
-                                    <td>{hotel['district']}</td>
+                    <div>
+                        <FilterSearchBar
+                            filterFunction={filterHandler}
+                            term={searchedWord}
+                        />
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>HOTEL</th>
+                                    <th>TELEPHONE</th>
+                                    <th>EMAIL</th>
+                                    <th>DISTRICT</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {searchedResult.map((hotel) => (
+                                    <tr key={hotel['id']}>
+                                        <td>{hotel['name']}</td>
+                                        <td>{hotel['userContactModel']['telephone']}</td>
+                                        <td>{hotel['userContactModel']['email']}</td>
+                                        <td>{hotel['district']}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
             }
         </div>
     );
