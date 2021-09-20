a6c3d42d9cea0970fa7c9d3fb9ad86ea6161cfd1
HiruniHasara <63002515+HiruniHasara@users.noreply.github.com>
2021-09-20 16:57:16 +0530

    request card componenet modified

--- src/components/hotel/newRequestHotel.js
+++ src/components/hotel/newRequestHotel.js
@@ -1,35 +1,74 @@
-import "../../css/newRequestPlace.css";
 import React from "react";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import ForwardIcon from "@material-ui/icons/Forward";
 import placePic from "../../assets/images/h4.jpg";
+import {Avatar, makeStyles} from '@material-ui/core';
+import axios from "../../service/axios";
+import Swal from 'sweetalert2';
 
-function NewRequestHotel() {
+const useStyles= makeStyles((theme) => ({
+  profilePhoto:{
+    minHeight: '150px',
+    minWidth: '150px'
+  }
+}))
+
+function NewRequestHotel(props) {
+  const classes = useStyles();
+
+  function approveRequestHandler() {
+    axios.put(`/approveHotelRequest/${props.id}`).then((result) => {
+      if(result.data==1){
+        Swal.fire({
+          icon: 'success',
+          text: 'Approve Success!',
+        })
+        // window.location.reload(false);
+      }else{
+        Swal.fire({
+          icon: 'error',
+          text: 'Approving Failed!',
+        })
+      }
+    });
+  }
+
+  function rejectRequestHandler(){
+    axios.delete(`/rejectHotelRequest/${props.id}`).then((result) => {
+      if(result.data==1){
+        Swal.fire({
+          icon: 'success',
+          text: 'Account Rejected!',
+        })
+      }else{
+        Swal.fire({
+          icon: 'error',
+          text: 'Rejection Failed!',
+        })
+      }
+    });
+  }
+
   return (
-    <div className="newRequestItem">
+    <div className="request-card">
       <div>
-        Hotel Name: <br />
-        Request Date: <br />
+        <Avatar className={classes.profilePhoto} alt="profile" variant="rounded" src={props.photo} />
       </div>
       <div>
-        Mount Lavinia Hotel <br />
-        2021.05.23 <br />
+        <p>Hotel Name: <span className="span-value">{props.name}</span></p>
+        <p>Registration NO.: <span className="span-value">{props.register}</span></p>
+        <p>District: <span className="span-value">{props.district}</span></p>
+        <p>Email: <span className="span-value">{props.email}</span></p>
+        <p>Telephone: <span className="span-value">{props.telephone}</span></p>
       </div>
       <div>
-        <img src={placePic} width="60px" height="60px" />
-      </div>
-      <div>
-        <button>
-          <CheckCircleIcon style={{ color: "green" }}></CheckCircleIcon> Approve
+        <button className="approve-button" onClick={approveRequestHandler}>
+          Approve
         </button>
-        <button className="clrSpan">
-          <DeleteForeverIcon style={{ color: "red" }}></DeleteForeverIcon>{" "}
-          Delete
+        <button className="reject-button" onClick={rejectRequestHandler}>
+          Reject
         </button>
-      </div>
-      <div className="forwardIcon">
-        <ForwardIcon></ForwardIcon>
       </div>
     </div>
   );
